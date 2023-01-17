1880f0297c7d85b1c1c15b8e2dfbf8cf95f247f6
Ahmed Emad <58553741+ahmed834@users.noreply.github.com>
2023-01-17 14:14:28 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById("Btn");
+var light = document.getElementById("lighe");
+
+function toggleBtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
